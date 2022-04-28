de9dadf63e275b15448243ece9f0e967fada387a
nio74 <mpaolo74@gmail.com>
2022-04-28 07:35:54 +0200

    casa28_04_07:30

--- lib/page/ins_mod_repair.dart
+++ lib/page/ins_mod_repair.dart
@@ -1,6 +1,8 @@
+import 'package:au79_web/blocs/clients/clients_bloc.dart';
 import 'package:au79_web/widgets/drawer_custom_widget.dart';
 import 'package:easy_autocomplete/easy_autocomplete.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class InsModRepair extends StatefulWidget {
   InsModRepair({Key? key}) : super(key: key);
@@ -20,16 +22,39 @@ class _InsModRepairState extends State<InsModRepair> {
           Row(children: [
             Expanded(
               child: Card(
-                child: EasyAutocomplete(
-                    suggestions: clients.map((e) => e.name).toList(),
-                    onChanged: (value) {
-                      print('onChange Value: $value');
-                    }),
+                child: EasyAutocompleteWidget(),
               ),
             )
           ])
         ],
       ),
+    );
+  }
+}
+
+class EasyAutocompleteWidget extends StatelessWidget {
+  const EasyAutocompleteWidget({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return BlocBuilder<ClientsBloc, ClientsState>(
+      builder: (context, state) {
+        if (state is ClientsLoading) {
+          return const Center(
+            child: CircularProgressIndicator(),
+          );
+        } else {
+          final clients = (state as ClientsLoaded).clients;
+          return EasyAutocomplete(
+              suggestions: clients.map((e) => e.name).toList(),
+              onChanged: (value) {
+                print('onChange Value: $value');
+              });
+        }
+        ;
+      },
     );
   }
 }
