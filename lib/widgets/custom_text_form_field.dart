3d1c6656588a4f2559ed126762d4ddbfd7391002
nio74 <mpaolo74@gmail.com>
2022-05-06 07:50:46 +0200

    casa0605

--- lib/widgets/custom_text_form_field.dart
+++ lib/widgets/custom_text_form_field.dart
@@ -1,34 +1,40 @@
 import 'package:flutter/material.dart';
 
 class CustomTextFormField extends StatelessWidget {
-  const CustomTextFormField({
+  CustomTextFormField({
     Key? key,
     int? maxline,
     required TextEditingController controller,
     required String txtLable,
+    required errorText,
   })  : _controller = controller,
         _txtLable = txtLable,
         _maxLine = maxline,
+        _errorText = errorText,
         super(key: key);
 
   final int? _maxLine;
   final TextEditingController _controller;
+  final String? _errorText;
   final String _txtLable;
 
   @override
   Widget build(BuildContext context) {
     return TextFormField(
       controller: _controller,
+
       //keyboardType: TextInputType.name,
       decoration: InputDecoration(
-          border: const OutlineInputBorder(), label: Text(_txtLable)),
+          errorText: _errorText,
+          border: const OutlineInputBorder(),
+          label: Text(_txtLable)),
       maxLines: _maxLine,
-      validator: (value) {
+      /* validator: (value) {
         if (value == null || value.isEmpty) {
           return '$_txtLable cannot be empty ';
         }
         return null;
-      },
+      }, */
     );
   }
 }
