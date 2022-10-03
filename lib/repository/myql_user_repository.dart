1dff5b6181131609d4da94b87b4a9ae03a24207c
nio74 <mpaolo74@gmail.com>
2022-10-03 13:35:46 +0200

    casa 310pomeriggio

--- lib/repository/myql_user_repository.dart
+++ lib/repository/myql_user_repository.dart
@@ -1,3 +1,10 @@
 import 'package:mysql1/mysql1.dart';
 
-class MysqlUserrepository {}
+class MysqlUserrepository {
+  var settings = ConnectionSettings(
+      host: 'https://paolomazzon.altervista.org/localhost',
+      port: 21,
+      user: 'paolomazzon',
+      password: '697477.altervistA',
+      db: 'my_paolomazzon ');
+}
